a574e1c690a99f15b4e6b683c2fede7f4589e6d9
Ubuntu <ubuntu@ip-172-31-41-104.ap-south-1.compute.internal>
2026-01-01 08:30:45 +0000

    Updated the Main.java

--- src/main/java/com/vaibhav/app/Main.java
+++ src/main/java/com/vaibhav/app/Main.java
@@ -1,11 +1,12 @@
 package com.vaibhav.app;
 
 public class Main {
-    public static void main (String[] args) {
+    public static void main (String[] args) throws InterruptedException {
         System.out.println("Hello from maven + GitLab CI");
 
         // Keep the alive
         while(true) {
+	    System.out.println("App is still running...");
             Thread.sleep(10000);
         }
     }
